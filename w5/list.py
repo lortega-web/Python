8ef8f58f8e69ea69750f2c61480af010b1a1e6e6
lortega <chois789@hotmail.com>
2021-01-05 19:34:07 +0000

    tareas

---
+++ w5/list.py
@@ -0,0 +1,20 @@
+# files
+# dna example
+#
+
+nucleotides = ["A", "C", "T", "G"]
+
+nucl_complement = {
+               "A":"T", 
+               "C":"G", 
+               "T":"A",
+               "G":"C" 
+	      }
+
+rna_transcription = {
+	       "A":"U",
+	       "C":"G",
+               "T":"A",
+               "G":"C"
+	      }
+
